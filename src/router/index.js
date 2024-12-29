06f17cd079d47cc9cb58a1cdcbc892c4ae13625b
choi-won-ik <whwwhs7837@gmail.com>
2024-12-29 16:35:07 +0900

    [Feat] payment페이지 추가가 #[feat] 결제정보 페이지 추가

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,11 @@ import findIdPw from '../pages/member/findIdPw.vue';
 import Review from '../pages/member/Review.vue';
 import Chat from '../pages/chat/Chat.vue';
 import ChatRoomList from '../pages/chat/ChatRoomList.vue';
+import Myproduct_home from '../pages/mypage/Myproduct_home.vue';
+import MystoreReviews from '../pages/mypage/MystoreReviews.vue';
+import Myproductstores from '../pages/mypage/Myproductstores.vue';
+import paymentList from '../pages/payment/paymentList.vue'
+import paymentDetails from '../pages/payment/paymentDetails.vue'
 import MyProduct from '../pages/mypage/MyProduct.vue';
 
 
@@ -30,6 +35,18 @@ const routes = [
   { path: '/chat', component: Chat},
   { path: '/chat/:id', component: ChatRoomList, props: true },
   { path: '/myhome', component: MyProduct},
+  { path: '/paymentList', component: paymentList },
+  { path: '/paymentDetails/:num', component: paymentDetails },
+
+  //Myproduct_home 내 스토어들, 내 스토어의 리뷰들을 볼 수 있는 라우터 경로
+  { path: "/myproduct_home", name: "Myproduct_home", component: Myproduct_home, 
+    children: [
+      { path: "myproductstores", name: 'Myproductstores', component: Myproductstores,
+       },
+      { path: "mystoreReviews", name: 'MystoreReviews', component: MystoreReviews,
+       },
+    ],
+  }
 ]
 
 const router = createRouter({
