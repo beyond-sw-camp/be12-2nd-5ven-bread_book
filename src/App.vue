e939e2d00e6541e56369303c8580565d9bcf3353
Hyorim K <37091594+daydeiday@users.noreply.github.com>
2024-12-29 23:03:01 +0900

    [Feat] App.vue router 수정, 특정 페이지들 Header, Footer 숨김처리, Login 액션 링크 수정 및 기타 경로 오류 수정, 컴포넌트 위치 정리 및 리팩토링

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <script setup>
+import Header from "./pages/common/Header.vue";
+import Footer from "./pages/common/Footer.vue";
+import { useRoute } from 'vue-router';
+const route = useRoute();
 </script>
 
 <template>
-  <!-- <router-link to="/login">login</router-link>
-  <br/> -->
-  <router-link to="/chat">chat</router-link>
-  <br/>
-  <router-link to="/myproduct_home/myproductstores">myproduct_home</router-link>
-  <br/>
-  
+  <Header v-if="route.meta.showHeader"></Header>
   <router-view></router-view>
+  <Footer v-if="route.meta.showFooter"></Footer>
 </template>
 
 <style scoped>
