cbd1c19a86b066e80ff5563cce91b2ea07673510
wkdlrn <kkjjgg97@gmail.com>
2024-12-29 21:47:24 +0900

    [Feat] 채팅방 모달 추가 및 로딩 화면 설정

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
+import { useLoadingStore } from './stores/useLoadingStore';
+
+const loadingStore = useLoadingStore()
 </script>
 
 <template>
   <!-- <router-link to="/login">login</router-link>
   <br/> -->
+  <div v-if="loadingStore.isLoading" class="loading-overlay">
+      <div class="spinner"></div>
+   </div>
   <router-link to="/chat">chat</router-link>
   <br/>
   <router-link to="/myproduct_home/myproductstores">myproduct_home</router-link>
