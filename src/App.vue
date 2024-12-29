18530608e4441dd29ad5957adc057cff5497fa13
LimChangeun <cksrms1030@naver.com>
2024-12-29 18:33:29 +0900

    충돌테스트

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
 </script>
 
 <template>
-  <router-link to="/login">login</router-link>
-  <br/>
+  <!-- <router-link to="/login">login</router-link>
+  <br/> -->
   <router-link to="/chat">chat</router-link>
   <br/>
   <router-link to="/myproduct_home/myproductstores">myproduct_home</router-link>
