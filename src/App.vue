7d3048f65f4c63d3b3c4aec0a066ee1ebd37eea1
LimChangeun <cksrms1030@naver.com>
2024-12-29 16:20:15 +0900

    [COmment] App.vue에 바로가기 링크 달기

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,6 @@ const route = useRoute();
 </script>
 
 <template>
-<<<<<<< HEAD
   <!-- <router-link to="/login">login</router-link>
   <br/> -->
   <div v-if="loadingStore.isLoading" class="loading-overlay">
@@ -18,17 +17,9 @@ const route = useRoute();
    <Header v-if="route.meta.showHeader"></Header>
    <router-view></router-view>
    <Footer v-if="route.meta.showFooter"></Footer>
-  
 
-=======
-  <router-link to="/chat">chat</router-link>
-  <br/>
-  <router-link to="/myproduct_home/myproductstores">myproduct_home</router-link>
-  <br/>
-  
-  <router-view></router-view>
 </template>
->>>>>>> 418b02f ([Feat, Init])
+
 
 <style scoped>
 
