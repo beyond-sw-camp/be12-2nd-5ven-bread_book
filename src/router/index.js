fcc69fe864e0fda2e736c46568146d15b7b3d931
wkdlrn <kkjjgg97@gmail.com>
2024-12-30 11:38:47 +0900

    [Fix] 병합 오류 수정

--- src/router/index.js
+++ src/router/index.js
@@ -15,19 +15,6 @@ import Myproductstores from '/src/pages/mypage/Myproductstores.vue';
 import SearchResult from "../pages/home/SearchResult.vue";
 import paymentList from "../pages/payment/paymentList.vue";
 import paymentDetails from "../pages/payment/paymentDetails.vue";
-import LoginView from "../pages/member/Login.vue"
-import Signup from '../pages/member/Signup.vue';
-import SignupSuccess from '../pages/member/SignupSuccess.vue';
-import findIdPw from '../pages/member/findIdPw.vue';
-import Review from '../pages/review/Review.vue';
-import Chat from '../pages/chat/Chat.vue';
-import ChatRoomList from '../pages/chat/ChatRoomList.vue';
-import Myproduct_home from '../pages/mypage/Myproduct_home.vue';
-import MystoreReviews from '../pages/mypage/MystoreReviews.vue';
-import Myproductstores from '../pages/mypage/Myproductstores.vue';
-import paymentList from '../pages/payment/paymentList.vue'
-import paymentDetails from '../pages/payment/paymentDetails.vue'
-import MyProduct from '../pages/mypage/MyProduct.vue';
 import ProductRegister from '../pages/productRegister/ProductRegister.vue';
 
 
@@ -42,14 +29,6 @@ const checkLogin = async (from, to, next) => {
 }
 
 const routes = [
-  { path: '/login', component: LoginView },
-  { path: '/signup', component: Signup},
-  { path: '/signupSuccess', component: SignupSuccess},
-  { path: '/findIdPw', component: findIdPw},
-  { path: '/review', component: Review},
-  { path: '/chat', component: Chat},
-  { path: '/chat/:id', component: ChatRoomList, props: true },
-  { path: '/myhome', component: MyProduct},
 
   {
     path: '/',
@@ -106,14 +85,23 @@ const routes = [
     props: true,
     meta: { showHeader: true, showFooter: true },
   },
+  { path: '/paymentList/:id', 
+    name: 'paymentList',
+    component: paymentList,
+    meta: { showHeader: true, showFooter: true },
+  },
+  { path: '/paymentDetails/:id',
+    name: 'paymentDetails',
+    component: paymentDetails,
+    meta: { showHeader: true, showFooter: true },  
+  },
   {
     path: '/productregister',
     name: 'ProductRegister',
     component: ProductRegister,
     meta: { showHeader: true, showFooter: true },
   },
-  { path: '/paymentList', component: paymentList },
-  { path: '/paymentDetails/:num', component: paymentDetails },
+
 
   //Myproduct_home 내 스토어들, 내 스토어의 리뷰들을 볼 수 있는 라우터 경로
   { path: "/myproduct_home", name: "Myproduct_home", component: Myproduct_home, 
