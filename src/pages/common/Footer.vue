145c81e58bd714e9a7b3c4d2cbc36874cdc79379
Hyorim K <37091594+daydeiday@users.noreply.github.com>
2025-01-01 07:57:51 +0900

    Minor changes in details, component composition

--- src/pages/common/Footer.vue
+++ src/pages/common/Footer.vue
@@ -1,11 +1,8 @@
-<script>
-export default {
-  name: "Footer",
-};
+<script setup>
 </script>
 <template>
   <!-- Footer -->
-  <footer class="py-6  bg-gray-200 text-gray-900">
+  <footer class="py-6 bg-gray-200 text-gray-900">
     <div class="container px-6 mx-auto space-y-6 divide-y divide-gray-400 md:space-y-12 divide-opacity-50">
       <div class="grid justify-center  lg:justify-between">
         <div class="flex flex-col self-center text-sm text-center md:block lg:col-start-1 md:space-x-6">
