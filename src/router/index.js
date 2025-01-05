7a221467af5d9cef09339716e90bde8daf26d0df
Hyorim K <37091594+daydeiday@users.noreply.github.com>
2025-01-05 23:42:41 +0900

    [Feat] Add scroll behavior when navigating via router-link

--- src/router/index.js
+++ src/router/index.js
@@ -147,6 +147,19 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: 'smooth',
+      }
+    }
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
 })
 
 export default router;
