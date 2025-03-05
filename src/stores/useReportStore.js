0e7e8af0f980d649dbd944d9e86514bc37d19875
LimChangeun <cksrms1030@naver.com>
2025-03-05 20:41:42 +0900

    [feat] 내_페이지_메인_리뷰,스토어_신고페이지구현

---
+++ src/stores/useReportStore.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { defineStore } from "pinia";
+
+export const useReportStore = defineStore('report', {
+  state: () => ({
+    reprotcreate: {},
+    reprotList: [],
+  }),
+  actions: {
+    async register(report, idx) {
+      const response = await axios.post("/api/reprot/create",
+        {
+          message: report.message,
+          productIdx: idx
+        }
+      );
+      return response;
+    },
+  }
+})
